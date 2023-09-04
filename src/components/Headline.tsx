10ff7c3911df79405172d889a67d661e206c8bc7
Neesh <avaneeshsamsi@gmail.com>
2023-09-04 21:36:54 +0530

    feat: Sections headings

---
+++ src/components/Headline.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/app/lib/utils"
+import Underline from "@/components/Graphics/Underline"
+
+export default function Headline({
+  className,
+  text,
+}: {
+  className: string
+  text: string
+}) {
+  return (
+    <h2 className={cn("relative font-serif", className)}>
+      <span>{text}</span>
+      <Underline className="absolute bottom-0 left-0 h-fit w-full" />
+    </h2>
+  )
+}
