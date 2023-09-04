687fe361be054ff2d0e852bf571f7971f855b479
Neesh <avaneeshsamsi@gmail.com>
2023-09-04 13:43:37 +0530

    feat: Hover fx & navLinks const

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,41 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function Navbar() {
+  const navLinks = [
+    {
+      text: "Home",
+      path: "/",
+    },
+    {
+      text: "Work",
+      path: "/work",
+    },
+    {
+      text: "About",
+      path: "/about",
+    },
+    {
+      text: "Contact",
+      path: "/contact",
+    },
+  ]
+
   return (
     <nav className="flex items-center justify-between py-8 text-lg">
-      <Link href="/" className="font-serif text-2xl font-medium">
+      <Link
+        href="/"
+        className="font-serif text-2xl font-medium transition-colors hover:text-lighter"
+      >
         Neesh Samsi
       </Link>
 
       <ul className="flex items-center justify-between gap-8 font-light">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/work">Work</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        {navLinks.map(({ text, path }) => (
+          <li key={path} className="transition-colors hover:text-lighter">
+            <Link href={path}>{text}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
-  );
+  )
 }
