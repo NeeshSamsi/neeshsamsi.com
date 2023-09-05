b1c454cbaf84b203332c1bdcca852c148238f377
Neesh <avaneeshsamsi@gmail.com>
2023-09-05 19:40:39 +0530

    feat: Reduce font size consistent will rest of page

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -6,8 +6,8 @@ export default async function AboutCard({
   paragraphs: string[]
 }) {
   return (
-    <div className="space-y-4 text-base lg:text-lg xl:text-xl">
-      <h3 className="font-serif text-2xl md:text-3xl">{heading}</h3>
+    <div className="space-y-4 text-sm md:text-base lg:text-lg xl:text-xl">
+      <h3 className="font-serif text-xl md:text-2xl md:text-3xl">{heading}</h3>
       {paragraphs.map((text, i) => (
         <p key={i}>{text}</p>
       ))}
