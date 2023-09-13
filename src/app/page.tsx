57bd5b49e8ba4faebc2da7aa58a317457977d88c
Neesh <avaneeshsamsi@gmail.com>
2023-09-13 17:04:51 +0530

    feat: Wave animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
   return (
     <>
       <main className="grid items-center justify-items-center gap-10 pt-8 sm:justify-items-start sm:pt-12 md:grid-cols-[1.3fr,1fr] md:gap-6 lg:grid-cols-2 lg:gap-24 xl:pt-4">
-        <div className="text-base font-light text-lighter sm:text-xl xl:text-2xl">
+        <div className="group text-base font-light text-lighter sm:text-xl xl:text-2xl">
           <p className="mb-2 flex items-center gap-2 lg:mb-3">
             <span>Hi</span>
             <Image
@@ -54,7 +54,7 @@ export default function Home() {
               height={48}
               width={48}
               sizes="(min-width: 1280px) 28px, 20px"
-              className="aspect-square w-5 object-cover xl:w-7"
+              className="animate-wave group-hover:animate-wave-y aspect-square w-5 object-cover xl:w-7"
             />
             <span>I&apos;m Neesh!</span>
           </p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,21 @@ export default {
         brand: "#F4A430",
         accent: "#25BBC5",
       },
+      animation: {
+        wave: "wave-rotate 1s ease-in-out infinite",
+        "wave-y":
+          "wave-rotate 1s ease-in-out infinite, wave-y 2s ease-in-out infinite",
+      },
+      keyframes: {
+        "wave-rotate": {
+          "0%, 100%": { rotate: "-5deg" },
+          "50%": { rotate: "5deg" },
+        },
+        "wave-y": {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-0.3rem)" },
+        },
+      },
     },
   },
   plugins: [typography],
