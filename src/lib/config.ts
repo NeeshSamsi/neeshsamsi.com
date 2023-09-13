b53d5b0832e387aac6fe1aec2faeebfb38f10db6
Neesh <avaneeshsamsi@gmail.com>
2023-09-13 15:24:21 +0530

    feat: Add placeholder about information

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -7,6 +7,27 @@ const config = {
   twitter: "@neeshsamsi",
   themeColor: "#1D3557",
   description: "",
+
+  about: [
+    {
+      heading: "Heading",
+      paragraphs: [
+        "Lorem ipsum dolor sit amet consectetur. Augue duis ornare risus eget purus habitant. Faucibus eu nunc nisl quis hendrerit at. Sit tempor quis elementum ultricies eget sagittis. Morbi aliquet sagittis id auctor rutrum diam diam et. Cursus.",
+      ],
+    },
+    {
+      heading: "Heading",
+      paragraphs: [
+        "Lorem ipsum dolor sit amet consectetur. Augue duis ornare risus eget purus habitant. Faucibus eu nunc nisl quis hendrerit at. Sit tempor quis elementum ultricies eget sagittis. Morbi aliquet sagittis id auctor rutrum diam diam et. Cursus.",
+      ],
+    },
+    {
+      heading: "Heading",
+      paragraphs: [
+        "Lorem ipsum dolor sit amet consectetur. Augue duis ornare risus eget purus habitant. Faucibus eu nunc nisl quis hendrerit at. Sit tempor quis elementum ultricies eget sagittis. Morbi aliquet sagittis id auctor rutrum diam diam et. Cursus.",
+      ],
+    },
+  ],
 }
 
 export default config
