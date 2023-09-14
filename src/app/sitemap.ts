ed7423d1a279698e363285af1a16080e7c9933e4
Neesh <avaneeshsamsi@gmail.com>
2023-09-14 17:24:54 +0530

    fix: Don't include tempalate document

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,10 +4,12 @@ import config from "@/lib/config"
 import { allWorks } from "contentlayer/generated"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const workUrls = allWorks.map((work) => ({
-    url: `${config.url}/work/${work.slug}`,
-    lastModified: new Date(work.updatedAt),
-  }))
+  const workUrls = allWorks
+    .filter((work) => work.slug !== "template")
+    .map((work) => ({
+      url: `${config.url}/work/${work.slug}`,
+      lastModified: new Date(work.updatedAt),
+    }))
 
   return [
     {
