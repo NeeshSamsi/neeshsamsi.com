a994210f30623b9dbba73c4aed557b3dc21c7c36
Neesh <avaneeshsamsi@gmail.com>
2023-09-14 18:40:57 +0530

    feat: Style prose links

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -120,7 +120,7 @@ export default function Work({ params }: Props) {
         )}
       </div>
 
-      <article className="prose prose-base mx-auto mt-12 text-light md:prose-lg xl:prose-xl marker:text-lighter prose-headings:font-serif prose-headings:font-normal prose-headings:text-light prose-img:w-full prose-img:max-w-none prose-img:rounded-xl prose-img:object-cover md:mt-16">
+      <article className="prose prose-base mx-auto mt-12 text-light md:prose-lg xl:prose-xl marker:text-lighter prose-headings:font-serif prose-headings:font-normal prose-headings:text-light prose-a:font-normal prose-a:text-lighter prose-a:underline-offset-2 prose-a:transition-colors hover:prose-a:text-brand prose-img:w-full prose-img:max-w-none prose-img:rounded-xl prose-img:object-cover md:mt-16">
         <MDXContent components={mdxComponents} />
         {liveUrl && (
           <div className="not-prose mt-6 w-fit text-base md:mt-10 md:text-lg xl:text-xl">
