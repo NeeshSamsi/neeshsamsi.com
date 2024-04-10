de364de4dc45ec94e592bb7b8a3cbbaccc440385
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-10 23:41:41 +0530

    feat: Keystatic init, complete client work & reviews collections

---
+++ keystatic.config.tsx
@@ -0,0 +1,117 @@
+import { collection, config, fields } from "@keystatic/core"
+import { image } from "node_modules/@keystatic/core/dist/declarations/src/form/fields"
+
+export default config({
+  storage: {
+    kind: "github",
+    repo: {
+      owner: "neeshsamsi",
+      name: "neeshsamsi.com",
+    },
+  },
+  ui: {},
+
+  singletons: {},
+
+  collections: {
+    clientwork: collection({
+      label: "Client Work",
+      path: "src/data/clientwork/*",
+      slugField: "title",
+      entryLayout: "content",
+      format: {
+        contentField: "content",
+      },
+
+      schema: {
+        published: fields.checkbox({ label: "Published", defaultValue: false }),
+        title: fields.slug({
+          name: {
+            label: "Title",
+            validation: { isRequired: true },
+          },
+          slug: {
+            label: "Slug",
+            description: "URL Friendly version of the title",
+          },
+        }),
+        description: fields.text({
+          label: "Description",
+          multiline: true,
+          validation: { isRequired: true },
+        }),
+        image: fields.image({
+          label: "Cover Image",
+          validation: { isRequired: true },
+          directory: "/public/clientwork/",
+          publicPath: "/clientwork/",
+        }),
+        imageAlt: fields.text({
+          label: "Image Alt Text",
+          description:
+            "This is read out to visually impaired users or shown as a placeholder when the image cannot load.",
+          validation: { isRequired: true },
+        }),
+        tags: fields.text({
+          label: "Tags",
+          validation: { isRequired: true },
+        }),
+        ctaText: fields.text({
+          label: "Secondary CTA Text",
+          validation: { isRequired: true },
+        }),
+        ctaLink: fields.url({
+          label: "Secondary CTA Link",
+          validation: { isRequired: true },
+        }),
+        updatedAt: fields.date({
+          label: "Updated At",
+          validation: { isRequired: true },
+          defaultValue: new Date().toString(),
+        }),
+        content: fields.mdx({
+          label: "Content",
+          components: {},
+          options: {
+            table: false,
+            code: false,
+            codeBlock: false,
+          },
+          extension: "mdx",
+        }),
+      },
+    }),
+
+    reviews: collection({
+      label: "Reviews & Feedback",
+      path: "src/data/reviews/*",
+      format: "json",
+      slugField: "name",
+
+      schema: {
+        name: fields.slug({
+          name: { label: "Name", validation: { isRequired: true } },
+          slug: {
+            label: "Slug",
+            description: "URL Friendly version of the name",
+          },
+        }),
+        designation: fields.text({
+          label: "Designation",
+          validation: { isRequired: true },
+        }),
+        avatar: fields.image({
+          label: "Avatar",
+          validation: { isRequired: true },
+          directory: "/public/reviews/",
+          publicPath: "/reviews/",
+        }),
+        content: fields.text({
+          label: "Review",
+          multiline: true,
+          validation: { isRequired: true },
+        }),
+      },
+    }),
+  },
+})
---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,99 @@
+import "./globals.css"
+
+import type { Metadata, Viewport } from "next"
+import config from "@/lib/config"
+
+import { Poppins } from "next/font/google"
+import localFont from "next/font/local"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import Script from "next/script"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+  variable: "--font-poppins",
+})
+
+const sunset = localFont({
+  src: [
+    {
+      path: "../../assets/fonts/sunset-serial-light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/sunset-serial-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../assets/fonts/sunset-serial-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sunset",
+})
+
+const { url, siteName, twitter: creator, themeColor, description } = config
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor,
+}
+
+export const metadata: Metadata = {
+  metadataBase: new URL(url),
+  title: {
+    default: siteName,
+    template: `%s | ${siteName}`,
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    images: ["/og.png"],
+    url,
+    siteName,
+    type: "website",
+  },
+  twitter: {
+    title: siteName,
+    description,
+    images: ["/og.png"],
+    creator,
+    card: "summary",
+  },
+  alternates: {
+    canonical: "/",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${poppins.variable} ${sunset.variable} overscroll-x-none scroll-smooth bg-dark font-sans text-light selection:bg-brand/80 selection:text-dark`}
+    >
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          strategy="lazyOnload"
+          src="https://umami.neeshsamsi.com/script.js"
+          data-website-id="c98851cd-fce3-401d-aae0-6249488c14e1"
+        />
+      )}
+      <body className="mx-auto max-w-screen-xl px-8">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
