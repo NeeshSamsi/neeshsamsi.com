43ec72ed73240f6eccdef2161ffbbd5d1f10cbaa
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-04-20 13:06:33 +0530

    fix: Use keystatic in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,20 @@
 import { type MetadataRoute } from "next"
 
 import config from "@/lib/config"
-import { allWorks } from "contentlayer/generated"
+import reader from "@/lib/keystatic"
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const workUrls = allWorks
-    .filter((work) => work.slug !== "template")
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const clientwork = await reader.collections.clientwork.all()
+
+  if (!clientwork) throw new Error("Invalid Client Work Collection.")
+
+  const workUrls = clientwork
+    .filter((work) =>
+      process.env.NODE_ENV === "production" ? work.entry.published : true,
+    )
     .map((work) => ({
       url: `${config.url}/work/${work.slug}`,
-      lastModified: new Date(work.updatedAt),
+      lastModified: new Date(work.entry.updatedAt),
     }))
 
   return [
