cbe2c6f42715a020c3d945463f1fdf0d3b063fce
Neesh <avaneeshsamsi@gmail.com>
2024-10-24 18:55:44 +0200

    fix: Switch section for div

--- src/slices/RichText/index.tsx
+++ src/slices/RichText/index.tsx
@@ -16,13 +16,13 @@ const RichText = ({ slice }: RichTextProps): JSX.Element => {
   } = slice
 
   return (
-    <section
+    <div
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className="prose prose-base mx-auto mt-12 text-light md:prose-lg xl:prose-xl marker:text-lighter prose-headings:font-serif prose-headings:font-normal prose-headings:text-light prose-a:font-normal prose-a:text-lighter prose-a:underline-offset-2 prose-a:transition-colors hover:prose-a:text-brand prose-img:w-full prose-img:max-w-none prose-img:rounded-xl prose-img:object-cover md:mt-16"
     >
       <PrismicRichText field={content} />
-    </section>
+    </div>
   )
 }
 
