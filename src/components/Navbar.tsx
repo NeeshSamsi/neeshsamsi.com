94a351f662b50353ed605c20a3b436d1693230f9
Neesh <avaneeshsamsi@gmail.com>
2024-10-24 13:00:49 +0200

    feat: Fetch links from Prismic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,11 @@
+import { client } from "@/lib/prismic"
 import Link from "next/link"
+import { PrismicNextLink } from "@prismicio/next"
 
-export default function Navbar() {
-  const navLinks: { text: string; path: string; external?: boolean }[] = [
-    {
-      text: "Notes",
-      path: "https://inquisit.blog/authors/neesh",
-      external: true,
-    },
-    {
-      text: "Work",
-      path: "/#work",
-    },
-    {
-      text: "About",
-      path: "/#about",
-    },
-    {
-      text: "Contact",
-      path: "/#contact",
-    },
-  ]
+export default async function Navbar() {
+  const {
+    data: { navLinks },
+  } = await client.getSingle("settings")
 
   return (
     <nav className="flex flex-col items-center justify-between gap-4 py-6 text-base sm:flex-row sm:gap-0 sm:py-8 sm:text-lg">
@@ -31,13 +17,12 @@ export default function Navbar() {
       </Link>
 
       <ul className="flex items-start justify-between gap-4 font-light sm:items-center sm:gap-8">
-        {navLinks.map(({ text, path, external }) => (
-          <li key={path} className="transition-colors hover:text-lighter">
-            {external ? (
-              <a href={path}>{text}</a>
-            ) : (
-              <Link href={path}>{text}</Link>
-            )}
+        {navLinks.map(({ link }, i) => (
+          <li key={`nav-link-${i}`}>
+            <PrismicNextLink
+              field={link}
+              className=" transition-colors hover:text-lighter"
+            />
           </li>
         ))}
       </ul>
