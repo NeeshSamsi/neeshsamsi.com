7cc071777618ccc1f0facbd33510b6d667e6b028
Neesh <avaneeshsamsi@gmail.com>
2024-10-25 11:49:36 +0200

    feat: Use og image

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -25,6 +25,9 @@ export async function generateMetadata({
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
+    openGraph: {
+      images: [page.data.meta_image.url!],
+    },
   }
 }
 
