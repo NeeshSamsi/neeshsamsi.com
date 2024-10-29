c5dbd413f7fa225dfd0bab4d0bb055dc8562f0c4
Neesh <avaneeshsamsi@gmail.com>
2024-10-29 12:02:05 +0100

    feat: Wrapper for h1 & h2 elements

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -2,20 +2,37 @@ import { cn } from "@/lib/utils"
 import Image from "next/image"
 
 export default function Headline({
-  className,
+  element = "h2",
   text,
+  className,
 }: {
-  className?: string
+  element?: "h1" | "h2"
   text: string
+  className?: string
 }) {
+  const defaultStyles =
+    "relative mb-14 w-fit font-serif text-3xl sm:text-4xl xl:text-[2.75rem]"
+
+  switch (element) {
+    case "h1":
+      return (
+        <h1 className={cn(defaultStyles, "xl:text-5xl", className)}>
+          <Content text={text} />
+        </h1>
+      )
+    case "h2":
+      return (
+        <h2 className={cn(defaultStyles, className)}>
+          <Content text={text} />
+        </h2>
+      )
+  }
+}
+
+function Content({ text }: { text: string }) {
   return (
-    <h2
-      className={cn(
-        "relative mb-14 w-fit font-serif text-3xl sm:text-4xl xl:text-[2.75rem]",
-        className,
-      )}
-    >
-      <span className="xs:no-underline underline decoration-brand underline-offset-4">
+    <>
+      <span className="underline decoration-brand underline-offset-4 xs:no-underline">
         {text}
       </span>
       <Image
@@ -24,8 +41,8 @@ export default function Headline({
         width={356}
         height={22}
         sizes="(min-width: 1280px) 174px, (min-width: 640px) 142px, 118px"
-        className="xs:block absolute -bottom-1 left-0 -z-10 hidden h-3 w-full xl:-bottom-3"
+        className="absolute -bottom-1 left-0 -z-10 hidden h-3 w-full xs:block xl:-bottom-3"
       />
-    </h2>
+    </>
   )
 }
