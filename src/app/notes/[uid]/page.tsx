21504f963243b54fd9ac66630c1baba086402b26
Neesh <avaneeshsamsi@gmail.com>
2024-10-29 13:48:19 +0100

    feat: Add dynamic notes page

---
+++ src/app/notes/[uid]/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
+import { SliceZone } from "@prismicio/react"
+
+import { createClient } from "@/prismicio"
+import { components } from "@/slices"
+
+type Params = { uid: string }
+
+export default async function Page({ params }: { params: Params }) {
+  const client = createClient()
+  const page = await client
+    .getByUID("notes", params.uid)
+    .catch(() => notFound())
+
+  return <SliceZone slices={page.data.slices} components={components} />
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Params
+}): Promise<Metadata> {
+  const client = createClient()
+  const page = await client
+    .getByUID("notes", params.uid)
+    .catch(() => notFound())
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [page.data.meta_image.url!],
+    },
+  }
+}
+
+export async function generateStaticParams() {
+  const client = createClient()
+  const pages = await client.getAllByType("notes")
+
+  return pages.map((page) => {
+    return { uid: page.uid }
+  })
+}
