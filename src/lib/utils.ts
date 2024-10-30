bd86efc6bcee3fceb4ae67d79cdcdef3a971369c
Neesh <avaneeshsamsi@gmail.com>
2024-10-30 17:28:20 +0100

    feat: Add date formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: Date) {
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  })
+
+  return formatter.format(date)
+}
