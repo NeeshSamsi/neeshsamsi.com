d7e14948cd3b86119c2d727c432a1a407da9de50
Neesh <avaneeshsamsi@gmail.com>
2024-10-30 17:28:47 +0100

    feat: Individual notes page

--- src/app/notes/[uid]/page.tsx
+++ src/app/notes/[uid]/page.tsx
@@ -4,6 +4,9 @@ import { SliceZone } from "@prismicio/react"
 
 import { createClient } from "@/prismicio"
 import { components } from "@/slices"
+import Article from "@/components/Article"
+import { asText } from "@prismicio/client"
+import { formatDate } from "@/lib/utils"
 
 type Params = { uid: string }
 
@@ -13,7 +16,31 @@ export default async function Page({ params }: { params: Params }) {
     .getByUID("notes", params.uid)
     .catch(() => notFound())
 
-  return <SliceZone slices={page.data.slices} components={components} />
+  const {
+    last_publication_date: pubDate,
+    data: { title, tags },
+  } = page
+
+  return (
+    <>
+      <main className="wrapper">
+        <div className="mx-auto mt-8 max-w-[65ch] space-y-3 text-center md:mt-12 md:space-y-6">
+          <h1 className="text-balance font-serif text-3xl font-medium text-brand md:text-4xl xl:text-5xl">
+            {asText(title)}
+          </h1>
+          <div className="flex items-center justify-center gap-2 text-base font-light text-light sm:text-lg md:text-xl xl:text-2xl">
+            <p>{tags}</p>
+            <div className="size-1 rounded-full bg-lighter" />
+            <p>{formatDate(new Date(pubDate))}</p>
+          </div>
+        </div>
+
+        <Article>
+          <SliceZone slices={page.data.slices} components={components} />
+        </Article>
+      </main>
+    </>
+  )
 }
 
 export async function generateMetadata({
