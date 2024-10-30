e823c2199d5aa97a08bc3927330b4d5256ab9b42
Neesh <avaneeshsamsi@gmail.com>
2024-10-30 15:23:00 +0100

    feat: Upgrade packages, prismic fix Next.js 15 compatibility

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -7,11 +7,7 @@ import { SliceZone } from "@prismicio/react"
 
 import { components } from "../../slices"
 
-// Next 15 async searchParams break SliceSimulatorParams type
-// export default async function SliceSimulatorPage(props: SliceSimulatorParams) {
-export default async function SliceSimulatorPage(props: {
-  searchParams: Promise<{ state?: string | undefined }>
-}) {
+export default async function SliceSimulatorPage(props: SliceSimulatorParams) {
   const searchParams = await props.searchParams
   const slices = getSlices(searchParams.state)
 
