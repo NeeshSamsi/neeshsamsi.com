3b12fe47c968373331448d7af86b7f2f7558a818
Neesh <avaneeshsamsi@gmail.com>
2024-10-30 18:21:15 +0100

    feat: Add static pages & dynamic notes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,17 +5,31 @@ import { client } from "@/lib/prismic"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const work = await client.getAllByType("work")
+  const notes = await client.getAllByType("notes")
 
   const workUrls = work.map(({ uid, data: { pubDate } }) => ({
     url: `${url}/work/${uid}`,
     lastModified: new Date(pubDate!),
   }))
+  const notesUrls = notes.map(({ uid, last_publication_date }) => ({
+    url: `${url}/notes/${uid}`,
+    lastModified: new Date(last_publication_date),
+  }))
 
-  return [
+  const staticUrls = [
     {
-      url,
+      url: `${url}/`,
       lastModified: new Date(),
     },
-    ...workUrls,
+    {
+      url: `${url}/work`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${url}/notes`,
+      lastModified: new Date(),
+    },
   ]
+
+  return [...staticUrls, ...workUrls, ...notesUrls]
 }
