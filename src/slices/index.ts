c711a46431d0b1040a1f58cec8b177bdc8e3f4ef
Neesh <avaneeshsamsi@gmail.com>
2024-10-30 16:45:27 +0100

    feat: Init CodeBlock slice

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic"
 
 export const components = {
   about: dynamic(() => import("./About")),
+  code_block: dynamic(() => import("./CodeBlock")),
   hero: dynamic(() => import("./Hero")),
   page_heading: dynamic(() => import("./PageHeading")),
   reviews: dynamic(() => import("./Reviews")),
