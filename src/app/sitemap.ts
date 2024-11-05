853ee14bbf6fa46913331926710c48dc8dc150a3
Neesh <avaneeshsamsi@gmail.com>
2024-11-05 19:32:54 +0100

    feat: Add play urls

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,11 +5,16 @@ import { client } from "@/lib/prismic"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const work = await client.getAllByType("work")
+  const play = await client.getAllByType("play")
   const notes = await client.getAllByType("notes")
 
   const workUrls = work.map(({ uid, data: { pubDate } }) => ({
     url: `${url}/work/${uid}`,
     lastModified: new Date(pubDate!),
+  }))
+  const playUrls = play.map(({ uid, last_publication_date }) => ({
+    url: `${url}/play/${uid}`,
+    lastModified: new Date(last_publication_date),
   }))
   const notesUrls = notes.map(({ uid, last_publication_date }) => ({
     url: `${url}/notes/${uid}`,
@@ -26,10 +31,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
     },
     {
+      url: `${url}/play`,
+      lastModified: new Date(),
+    },
+    {
       url: `${url}/notes`,
       lastModified: new Date(),
     },
   ]
 
-  return [...staticUrls, ...workUrls, ...notesUrls]
+  return [...staticUrls, ...workUrls, ...playUrls, ...notesUrls]
 }
