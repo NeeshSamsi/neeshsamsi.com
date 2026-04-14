b92af5ee030cc6a677323a49dbd7a31590004cb3
Neesh <avaneeshsamsi@gmail.com>
2026-04-14 18:18:35 +0530

    Feat: Update nav links to be socials

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,31 +3,36 @@ import Link from "next/link"
 import { PrismicNextLink } from "@prismicio/next"
 
 export default async function Navbar() {
-  const {
-    data: { navLinks },
-  } = await client.getSingle("settings")
+  const { data } = await client.getSingle("settings")
+
+  const navLinks = [
+    { id: "instagram", label: "Instagram", field: data.instagram },
+    { id: "youtube", label: "YouTube", field: data.youtube },
+    { id: "github", label: "GitHub", field: data.github },
+    { id: "linkedin", label: "LinkedIn", field: data.linkedin },
+  ]
 
   return (
-    <nav className="col-span-full grid grid-cols-subgrid">
-      <div className="col-span-full flex flex-col items-center justify-between gap-4 py-6 text-base sm:flex-row sm:gap-0 sm:py-8 sm:text-lg">
-        <Link
-          href="/"
-          className="font-serif text-lg font-medium transition-colors hover:text-lighter md:text-xl"
-        >
-          Neesh Samsi
-        </Link>
+    <nav className="col-span-full grid grid-cols-subgrid items-center justify-between gap-4 py-6 text-base sm:flex-row sm:gap-0 sm:py-8 sm:text-lg">
+      <Link
+        href="/"
+        className="font-serif text-lg font-medium transition-colors hover:text-lighter md:text-xl"
+      >
+        Neesh Samsi
+      </Link>
 
-        <ul className="flex items-start justify-between gap-4 font-light sm:items-center sm:gap-8">
-          {navLinks.map(({ link }, i) => (
-            <li key={`nav-link-${i}`}>
-              <PrismicNextLink
-                field={link}
-                className="underline-animation transition-colors hover:text-lighter"
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className="flex items-start justify-between gap-4 font-light sm:items-center sm:gap-8">
+        {navLinks.map(({ id, label, field }) => (
+          <li key={`nav-link-${id}`}>
+            <PrismicNextLink
+              field={field}
+              className="underline-animation transition-colors hover:text-lighter"
+            >
+              {label}
+            </PrismicNextLink>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
