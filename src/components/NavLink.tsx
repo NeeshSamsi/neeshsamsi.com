efecb4b3712f0b859ac410b154589d6039d15f40
Neesh <avaneeshsamsi@gmail.com>
2026-04-17 17:10:56 +0530

    fix: Formatting

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,15 +1,12 @@
 import { PrismicNextLink, type PrismicNextLinkProps } from "@prismicio/next"
 import { cn } from "@/lib/utils"
 
-export default function NavLink({
-  className,
-  ...props
-}: PrismicNextLinkProps) {
+export default function NavLink({ className, ...props }: PrismicNextLinkProps) {
   return (
     <PrismicNextLink
       className={cn(
         "relative inline-block transition-colors hover:text-lighter",
-        "after:absolute after:bottom-0 after:left-0 after:h-[0.075em] after:w-full after:bg-current after:opacity-0 after:-translate-y-[0.2em] after:transition-all after:duration-150 after:ease-in-out",
+        "after:absolute after:bottom-0 after:left-0 after:h-[0.075em] after:w-full after:-translate-y-[0.2em] after:bg-current after:opacity-0 after:transition-all after:duration-150 after:ease-in-out",
         "hover:after:translate-y-[0.1em] hover:after:opacity-100",
         "focus:after:translate-y-[0.1em] focus:after:opacity-100",
         className,
